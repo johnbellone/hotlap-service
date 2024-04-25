56cb36032cbba4ce73a8e5491b748b4aca2b1bc4
John Bellone <me@jbellone.dev>
2024-04-24 21:24:33 -0400

    sdk: add new module

---
+++ client/src/results.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+use std::io::{BufRead, BufReader};
+use tokio::fs::File;
+
+async fn read_results(file: File) -> Result<usize, std::io::Error> {
+    let file = file.into_std().await;
+    tokio::task::spawn_blocking(move || {}).await?;
+}
